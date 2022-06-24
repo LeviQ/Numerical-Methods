e03d84093e2da16570ac1e0b5e208279ceebb87b
Aleksander Lewicki <82654757+LeviQ@users.noreply.github.com>
2022-06-24 14:03:54 +0200

    Update U matrix solving function

--- U matrix solving function(U,B).py
+++ U matrix solving function(U,B).py
@@ -1,17 +1,17 @@
 import numpy as np
 
-### Rozwiazywanie układu rownan z macierza gorną trojkątną
-def Lewicki_Aleksander_ukladU(U,b):      ### U to macierz gorna, b - wektor wyrazow wolnych
-    A = [0] * len(U[0]) ### Nasza macierz poczatkowo wypełniona zerami
+### Solving the system of equations with the upper triangular matrix
+def U_matrix_solving_function(U,b):      ### U is the upper matrix, b - vector of intercepts
+    A = [0] * len(U[0]) ### Our matrix initially filled with zeros
     for i in range(len(U)):
         c = len(U) - i - 1
-        A[c] = b[c]     ### Uzupełnianie naszej macierzy
+        A[c] = b[c]     
         for j in range(i+1):
             d = len(U[c]) - 1 - j
             if c == d: continue
-            A[c] = A[c] - U[c][d] * A[d]    ### Wyliczanie poszczegolnych x
-        A[c] = A[c] / U[c][c]               ### ^^
+            A[c] = A[c] - U[c][d] * A[d]    
+        A[c] = A[c] / U[c][c]               
     return A
-### Testy rozwiązania macierzy:
-print(Lewicki_Aleksander_ukladU([[3, -4, 3], [0, 2, 3], [0, 0, -2]], [23, 2, -4]))
-print(Lewicki_Aleksander_ukladU([[4, 5, 2], [0, 5, 1], [0, 0, -6]], [15, 5, 12]))
+
+print(U_matrix_solving_function([[3, -4, 3], [0, 2, 3], [0, 0, -2]], [23, 2, -4]))
+print(U_matrix_solving_function([[4, 5, 2], [0, 5, 1], [0, 0, -6]], [15, 5, 12]))
